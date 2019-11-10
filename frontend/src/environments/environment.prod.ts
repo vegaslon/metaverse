6c35c1130d0ce75582d128e90504e44940015091
Maki <mxmcube@gmail.com>
2019-11-10 10:33:10 +0000

    Using captcha proper key in production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
 	// test key
-	reCaptchaSiteKey: "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI",
+	reCaptchaSiteKey: "6LeP87kUAAAAANyj9z9fmyLm1uFlVCoYnvucgu8g",
 };
