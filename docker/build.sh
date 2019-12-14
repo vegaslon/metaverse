33de931ff38e000ae1c625d5cf95b2a7c0dcac79
Maki <mxmcube@gmail.com>
2019-12-13 16:18:05 -0800

    Updated docker to work with ssr

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,15 @@
 rm -rf app
-mkdir app
+mkdir app app/frontend app/server
 
 cd ../frontend
-npm run build
+npm run build:ssr
+cp -r dist ../docker/app/frontend
 
 cd ../server
 npm run build
-cp -r dist ../docker/app
-cp -r assets ../docker/app
-cp -r frontend ../docker/app
-cp -r package.json ../docker/app
+cp -r dist ../docker/app/server
+cp -r assets ../docker/app/server
+cp -r package.json ../docker/app/server
 
 cd ../docker
 docker build -t tivolicloud/metaverse:latest .
