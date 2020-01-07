fb4c57f43cae41c7cfbcb5bcbf7584f89d9e4030
Maki <mxmcube@gmail.com>
2020-01-06 20:24:46 -0800

    Updated download component

--- frontend/src/app/header/download/download.component.ts
+++ frontend/src/app/header/download/download.component.ts
@@ -1,15 +1,45 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
 	selector: "app-download",
 	templateUrl: "./download.component.html",
 	styleUrls: ["./download.component.scss"],
 })
-export class DownloadComponent {
-	os = null;
+export class DownloadComponent implements OnInit {
+	readonly releasesPath = "https://cdn.tivolicloud.com/releases";
 
-	constructor() {
-		if (navigator.platform.indexOf("Win") != -1) this.os = "win";
-		if (navigator.platform.indexOf("Mac") != -1) this.os = "mac";
+	//os = null;
+
+	found = false;
+	filename = "";
+	filesize = "";
+
+	constructor(private http: HttpClient) {}
+
+	private bytesToMB(bytes: number) {
+		return Math.floor(bytes / 1000 / 1000);
+	}
+
+	ngOnInit() {
+		//if (navigator.platform.indexOf("Win") != -1) this.os = "win";
+		//if (navigator.platform.indexOf("Mac") != -1) this.os = "mac";
+
+		this.http
+			.get(this.releasesPath + "/latest.yml", { responseType: "text" })
+			.subscribe(
+				ymlStr => {
+					const matches = ymlStr.match(
+						/files:[^]*?url: ([^]*?)\n[^]*?size: ([^]*?)\n/,
+					);
+					if (matches.length < 3) return;
+
+					this.found = true;
+					this.filename = matches[1];
+					this.filesize =
+						this.bytesToMB(parseInt(matches[2])) + " MB";
+				},
+				err => {},
+			);
 	}
 }
