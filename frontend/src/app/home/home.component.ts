ad6bd740cf52db502d74413c36865b57c887e98c
Maki <mxmcube@gmail.com>
2020-01-16 14:31:04 -0800

    More work on the website!

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,12 +1,39 @@
-import { Component, OnInit } from "@angular/core";
+import {
+	Component,
+	OnInit,
+	ViewChild,
+	ElementRef,
+	OnDestroy,
+} from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { DownloadComponent } from "../header/download/download.component";
 
 @Component({
 	selector: "app-home",
 	templateUrl: "./home.component.html",
 	styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
-	constructor() {}
+export class HomeComponent implements OnInit, OnDestroy {
+	@ViewChild("video", { static: true }) videoEl: ElementRef;
+	video: HTMLVideoElement;
 
-	ngOnInit() {}
+	videoInterval: NodeJS.Timer;
+
+	constructor(public dialog: MatDialog) {}
+
+	ngOnInit() {
+		this.video = this.videoEl.nativeElement;
+
+		this.videoInterval = setInterval(() => {
+			if (this.video.paused) this.video.play();
+		}, 100);
+	}
+
+	ngOnDestroy() {
+		clearInterval(this.videoInterval);
+	}
+
+	openDownload() {
+		this.dialog.open(DownloadComponent);
+	}
 }
