56175bb2b98e1b0c8b0eec101829cc4f8c6e0b1c
Maki <mxmcube@gmail.com>
2020-02-05 17:14:38 -0800

    Updated docker pipeline to integrate with .gitlab-ci.yml

--- docker/mongodump.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker-compose exec db mongodump -d metaverse -o /data/db
-sudo rm -rf mongodump.old
-sudo mv mongodump mongodump.old
-sudo mv db/metaverse mongodump
--- docker/upload.sh
+++
@@ -1,4 +0,0 @@
-#docker save tivolicloud/metaverse:latest | ssh -C -p 9001 maki@tivolicloud.com docker load
-
-docker save tivolicloud/metaverse:latest | gzip | pv | \
-ssh -p 9001 maki@tivolicloud.com docker load
