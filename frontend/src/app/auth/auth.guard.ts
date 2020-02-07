33c4dd3bc5672cce8eaef482e0cab91a2f53c03f
Maki <mxmcube@gmail.com>
2020-02-07 11:46:31 -0800

    Updated auth guard

--- frontend/src/app/auth/auth.guard.ts
+++ frontend/src/app/auth/auth.guard.ts
@@ -9,7 +9,7 @@ import {
 	UrlTree,
 } from "@angular/router";
 import { Observable } from "rxjs";
-import { map, mergeMap } from "rxjs/operators";
+import { map, mergeMap, filter, tap } from "rxjs/operators";
 import { SignInComponent } from "../header/sign-in/sign-in.component";
 import { AuthService } from "./auth.service";
 
@@ -28,52 +28,36 @@ export class AuthGuard implements CanActivate {
 		route: ActivatedRouteSnapshot,
 		state: RouterStateSnapshot,
 	): Observable<boolean | UrlTree> {
-		const afterLoggingIn = (): Observable<unknown> =>
-			new Observable(sub => {
-				// check if still logging in
-
-				if (this.authService.loggingIn$.value) {
-					const loggingInSub = this.authService.loggingIn$.subscribe(
-						loggingIn => {
-							if (loggingIn != false) return; // wait until its false
-							loggingInSub.unsubscribe();
-							sub.next();
-						},
-					);
-				} else {
-					sub.next();
-				}
-			});
-
-		const handleUser = (): Observable<boolean | UrlTree> => {
-			// if user, continue to page!
-
-			const user = this.authService.user$.value;
-			if (user != null) return new Observable(sub => sub.next(true));
-
-			// if not, login popup
-
-			const dialog = this.dialog.open(SignInComponent, {
-				width: "400px",
-			});
-
-			return dialog.afterClosed().pipe(
-				mergeMap(() => afterLoggingIn()),
-				map(() => {
-					const user = this.authService.user$.value;
-
-					return user != null
-						? true
-						: this.router.createUrlTree(["/"]);
-				}),
-			);
-		};
-
 		if (isPlatformServer(this.platform))
 			return new Observable(sub =>
 				sub.next(this.router.createUrlTree(["/"])),
 			);
 
-		return afterLoggingIn().pipe(mergeMap(() => handleUser()));
+		return this.authService.loggingIn$.pipe(
+			// only when logging is has finished
+			filter(loggingIn => loggingIn == false),
+			mergeMap(() =>
+				((): Observable<boolean | UrlTree> => {
+					// if user, continue to page!
+					const user = this.authService.user$.value;
+					if (user != null)
+						return new Observable(sub => sub.next(true));
+
+					// if not, login popup
+					const dialog = this.dialog.open(SignInComponent, {
+						width: "400px",
+					});
+
+					return dialog.afterClosed().pipe(
+						map(() => {
+							const user = this.authService.user$.value;
+							return user != null
+								? true
+								: this.router.createUrlTree(["/"]);
+						}),
+					);
+				})(),
+			),
+		);
 	}
 }
