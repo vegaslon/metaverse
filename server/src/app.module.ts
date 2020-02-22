75f78f458144fc31d0fbf7df344840a71a4cb51c
Maki <mxmcube@gmail.com>
2020-02-21 16:26:41 -0800

    Added health checks

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import { TerminusModule } from "@nestjs/terminus";
 import { SentryModule } from "@ntegral/nestjs-sentry";
 import { AdminModule } from "./admin/admin.module";
 import { ApiDomainsModule } from "./api/domains/domains.module";
@@ -7,14 +8,15 @@ import { ApiPlacesModule } from "./api/places/places.module";
 import { ApiUserStoriesModule } from "./api/user-stories/user-stories.module";
 import { ApiUserModule } from "./api/user/user.module";
 import { ApiUsersModule } from "./api/users/users.module";
+import { AppController } from "./app.controller";
 import { AuthModule } from "./auth/auth.module";
 import { DomainModule } from "./domain/domain.module";
 import { EmailModule } from "./email/email.module";
 import { DB_HOST, DB_NAME, DB_PASS, DB_USER, DEV } from "./environment";
+import { HealthService } from "./health.service";
 import { PuppeteerModule } from "./puppeteer/puppeteer.module";
 import { UserModule } from "./user/user.module";
 import { VideoStreamModule } from "./video-stream/video-stream.module";
-import { AppController } from "./app.controller";
 
 @Module({
 	imports: [
@@ -39,6 +41,10 @@ import { AppController } from "./app.controller";
 			dbName: DB_NAME,
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
+		}),
+
+		TerminusModule.forRootAsync({
+			useClass: HealthService,
 		}),
 
 		EmailModule,
---
+++ server/src/health.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from "@nestjs/common";
+import { InjectConnection } from "@nestjs/mongoose";
+import {
+	DiskHealthIndicator,
+	DNSHealthIndicator,
+	MemoryHealthIndicator,
+	MongooseHealthIndicator,
+	TerminusEndpoint,
+	TerminusModuleOptions,
+	TerminusOptionsFactory,
+} from "@nestjs/terminus";
+import { Connection } from "mongoose";
+
+@Injectable()
+export class HealthService implements TerminusOptionsFactory {
+	constructor(
+		private readonly dns: DNSHealthIndicator,
+		@InjectConnection() private mongooseConnection: Connection,
+		private readonly mongoose: MongooseHealthIndicator,
+		private readonly memory: MemoryHealthIndicator,
+		private readonly disk: DiskHealthIndicator,
+	) {}
+
+	createTerminusOptions(): TerminusModuleOptions {
+		const healthEndpoint: TerminusEndpoint = {
+			url: "/health",
+			healthIndicators: [
+				async () => this.dns.pingCheck("google", "https://google.com"),
+				async () =>
+					this.mongoose.pingCheck("mongoose", {
+						connection: this.mongooseConnection,
+					}),
+				async () => this.memory.checkRSS("memory", 150 * 1024 * 1024),
+				// async () => this.disk.checkStorage("disk", {}),
+			],
+		};
+
+		return {
+			endpoints: [healthEndpoint],
+		};
+	}
+}
