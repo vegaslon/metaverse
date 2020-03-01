42b0a7fcaec70f481c8714ead6ca604a21ab2dc3
Maki <mxmcube@gmail.com>
2020-03-01 13:27:28 -0800

    Updated formatExt

--- frontend/src/app/utils.service.ts
+++ frontend/src/app/utils.service.ts
@@ -4,6 +4,121 @@ import { Injectable } from "@angular/core";
 	providedIn: "root",
 })
 export class UtilsService {
+	fileTypes = {
+		image: [
+			// https://en.wikipedia.org/wiki/Image_file_formats
+			"jpeg",
+			"jpg",
+			"jfif",
+			"exif",
+			"tiff",
+			"tif",
+
+			"gif",
+			"bmp",
+			"png",
+			"apng",
+
+			"ppm",
+			"pgm",
+			"pbm",
+			"pnm",
+
+			"webp",
+			"hdr",
+			"heif",
+
+			"flif",
+			"ico",
+			"tga",
+
+			"afphoto",
+			"kra",
+			"psd",
+			"sai",
+			"xcf",
+
+			"svg",
+			"afdesign",
+			"ai",
+
+			"dds",
+			"ktx",
+		],
+		code: [
+			// https://en.wikipedia.org/wiki/List_of_file_formats#Programming_languages_and_scripts
+			"bat",
+			"cmd",
+			"coffee",
+			"c",
+			"cpp",
+			"h",
+			"hpp",
+			"js",
+			"jsx",
+			"lua",
+			"php",
+			"pl",
+			"ps1",
+			"py",
+			"rb",
+			"rs",
+			"sh",
+			"ts",
+			"tsx",
+
+			"json",
+			"xml",
+			"yaml",
+			"yml",
+
+			"glsl",
+			"fs",
+			"vs",
+
+			"html",
+			"css",
+		],
+		video: [
+			// https://en.wikipedia.org/wiki/List_of_file_formats#Video
+			"avi",
+			"flv",
+			"mkv",
+			"mov",
+			"mpeg",
+			"mpg",
+			"ogv",
+			"wmv",
+			"webm",
+			"mp4",
+		],
+		audio: [
+			// https://en.wikipedia.org/wiki/List_of_file_formats#Sound_and_music
+			"aiff",
+			"raw",
+			"wav",
+			"flac",
+			"wma",
+			"brstm",
+			"mp3",
+			"aac",
+			"ogg",
+		],
+		model: [
+			// https://en.wikipedia.org/wiki/Image_file_formats 3d vector formats
+			"glb",
+			"gltf",
+			"fbx",
+			"obj",
+			"stl",
+			"dae",
+			"mmd",
+			"ply",
+		],
+		text: ["txt", "doc", "docx", "mobi", "pdf", "epub", "odt"],
+		font: ["bdf", "otf", "ttf", "woff"],
+	};
+
 	constructor() {}
 
 	formatBytes(b: number) {
@@ -48,55 +163,25 @@ export class UtilsService {
 	// }
 
 	formatExt(fileName: string) {
-		switch (
-			fileName
-				.split(".")
-				.pop()
-				.toLowerCase()
-		) {
-			case "png":
-			case "jpg":
-			case "jpeg":
-			case "gif":
-			case "ktx":
-			case "apng":
-			case "bmp":
-			case "tga":
-			case "tif":
-			case "tiff":
-			case "svg":
-			case "dds":
-				return { type: "image", icon: "insert_photo" };
-			case "js":
-			case "jsx":
-			case "ts":
-			case "tsx":
-			case "cpp":
-			case "c":
-			case "hpp":
-			case "h":
-			case "json":
-			case "glsl":
-			case "fs":
-			case "vs":
-				return { type: "code", icon: "code" };
-			case "mp4":
-			case "ogv":
-			case "webm":
-			case "avi":
-				return { type: "video", icon: "videocam" };
-			case "glb":
-			case "gltf":
-			case "fbx":
-			case "obj":
-			case "stl":
-			case "dae":
-			case "mmd":
-			case "ply":
-				return { type: "model", icon: "3d_rotation" };
-			default:
-				return { type: "file", icon: "insert_drive_file" };
+		const ext = fileName
+			.split(".")
+			.pop()
+			.toLowerCase();
+
+		for (const type of Object.keys(this.fileTypes)) {
+			if (this.fileTypes[type].includes(ext)) {
+				if (type == "image") return { type, icon: "insert_photo" };
+				if (type == "code") return { type, icon: "code" };
+				if (type == "video") return { type, icon: "videocam" };
+				if (type == "audio") return { type, icon: "audiotrack" };
+				if (type == "model") return { type, icon: "3d_rotation" };
+				if (type == "text") return { type, icon: "description" };
+				if (type == "font") return { type, icon: "text_format" };
+				return { type, icon: "insert_drive_file" };
+			}
 		}
+
+		return { type: "file", icon: "insert_drive_file" };
 	}
 
 	displayPlural = (n: number, singular: string, plural?: string) =>
