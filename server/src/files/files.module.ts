aac98b525d429bc34e60fc7f025f994065d19b5d
Maki <mxmcube@gmail.com>
2020-03-10 19:18:52 +0000

    Experimental WebDav testing

--- server/src/files/files.module.ts
+++ server/src/files/files.module.ts
@@ -5,10 +5,22 @@ import { FilesHostController } from "./files-host.controller";
 import { FilesHostService } from "./files-host.service";
 import { FilesController } from "./files.controller";
 import { FilesService } from "./files.service";
+import { WebDavService } from "./webdav.service";
+import { JwtModule } from "@nestjs/jwt";
+import { JWT_SECRET } from "../environment";
 
 @Module({
-	imports: [AuthModule, UserModule],
-	providers: [FilesService, FilesHostService],
+	imports: [
+		AuthModule,
+		UserModule,
+		JwtModule.register({
+			secret: JWT_SECRET,
+			signOptions: {
+				noTimestamp: true,
+			},
+		}),
+	],
+	providers: [FilesService, FilesHostService, WebDavService],
 	exports: [FilesService],
 	controllers: [FilesController, FilesHostController],
 })
--- server/src/files/webdav.service.ts
+++ server/src/files/webdav.service.ts
@@ -1,70 +1,14 @@
-// import { Injectable, OnModuleInit } from "@nestjs/common";
-// import { v2 as WebDav } from "webdav-server";
-// import { AuthService } from "../auth/auth.service";
-// import { FilesService } from "./files.service";
-// import { AuthTokenGrantType, AuthTokenScope } from "../auth/auth.dto";
-// import { User } from "../user/user.schema";
-// import { KeyedFileSystem, Folder } from "../common/keyed-file-system";
-
-// class UserManager implements WebDav.ITestableUserManager {
-// 	constructor(private readonly authService: AuthService) {}
-
-// 	getDefaultUser(callback: (user: WebDav.IUser) => void) {
-// 		console.log("getDefaultUser");
-// 		callback({
-// 			uid: "DefaultUser",
-// 			isAdministrator: false,
-// 			isDefaultUser: true,
-// 			username: "DefaultUser",
-// 			password: null,
-// 		});
-// 	}
-
-// 	getUserByNamePassword(
-// 		username: string,
-// 		password: string,
-// 		callback: (error: Error, user?: WebDav.IUser) => void,
-// 	) {
-// 		console.log("getUserByNamePassword");
-// 		this.authService
-// 			.validateUser({
-// 				username,
-// 				password,
-// 				grant_type: AuthTokenGrantType.password,
-// 				scope: AuthTokenScope.owner,
-// 			})
-// 			.then(user => {
-// 				callback(null, {
-// 					uid: user.id,
-// 					isAdministrator: false,
-// 					isDefaultUser: false,
-// 					username: user.username,
-// 					password: null,
-// 					user,
-// 				} as any);
-// 			})
-// 			.catch(error => {
-// 				callback(error);
-// 			});
-// 	}
-// }
-
-// class PrivilegeManager extends WebDav.PrivilegeManager {
-// 	constructor() {
-// 		super();
-// 	}
-
-// 	_can(
-// 		fullPath: WebDav.Path,
-// 		user: WebDav.IUser,
-// 		resource: WebDav.Resource,
-// 		privilege: string,
-// 		callback: WebDav.PrivilegeManagerCallback,
-// 	) {
-// 		//console.log("_can", user);
-// 		callback(null, user != null);
-// 	}
-// }
+import { Injectable, OnModuleInit } from "@nestjs/common";
+import { v2 as WebDav } from "webdav-server";
+import { AuthService } from "../auth/auth.service";
+import { FilesService } from "./files.service";
+import { AuthTokenGrantType, AuthTokenScope } from "../auth/auth.dto";
+import { User } from "../user/user.schema";
+import { KeyedFileSystem, Folder } from "../common/keyed-file-system";
+import { md5 } from "webdav-server/lib/user/CommonFunctions";
+import { JwtService } from "@nestjs/jwt";
+import { UserService } from "../user/user.service";
+import { JwtPayloadType, JwtPayload } from "../auth/jwt.strategy";
 
 // class FileSystem extends WebDav.FileSystem {
 // 	private fs = new KeyedFileSystem();
@@ -203,31 +147,157 @@
 // 	}
 // }
 
-// @Injectable()
-// export class WebDavService implements OnModuleInit {
-// 	private readonly port = 4000;
-// 	private server: WebDav.WebDAVServer;
+// based on WebDav.HTTPDigestAuthentication
+// https://github.com/OpenMarshal/npm-WebDAV-Server/blob/master/src/user/v2/authentication/HTTPDigestAuthentication.ts
+// class TivoliAuthentication implements WebDav.HTTPAuthentication {
+// 	private readonly realm = "Tivoli Cloud VR";
+// 	private readonly nonceSize = 50;
 
-// 	constructor(
-// 		private readonly authService: AuthService,
-// 		private readonly filesService: FilesService,
-// 	) {}
+// 	constructor(private readonly authService: AuthService) {}
 
-// 	onModuleInit() {
-// 		this.server = new WebDav.WebDAVServer({
-// 			port: this.port,
+// 	generateNonce(): string {
+// 		const buffer = Buffer.alloc(this.nonceSize);
+// 		for (let i = 0; i < buffer.length; ++i)
+// 			buffer[i] = Math.floor(Math.random() * 256);
 
-// 			//privilegeManager: new PrivilegeManager(),
-// 			httpAuthentication: new WebDav.HTTPBasicAuthentication(
-// 				new UserManager(this.authService),
-// 				"Tivoli Cloud VR",
-// 			),
-// 			requireAuthentification: true,
+// 		return md5(buffer);
+// 	}
 
-// 			rootFileSystem: new FileSystem(this.filesService),
-// 			//rootFileSystem: new WebDav.PhysicalFileSystem("/home/maki"),
-// 		});
+// 	askForAuthentication(ctx: WebDav.HTTPRequestContext) {
+// 		return {
+// 			"WWW-Authenticate": `Digest realm="${
+// 				this.realm
+// 			}", qop="auth", nonce="${this.generateNonce()}", opaque="${this.generateNonce()}"`,
+// 		};
+// 	}
 
-// 		this.server.start(() => console.log("WebDav server running on *:4000"));
+// 	getUser(
+// 		ctx: WebDav.HTTPRequestContext,
+// 		callback: (error: Error, user: WebDav.IUser) => void,
+// 	) {
+// 		const o
 // 	}
 // }
+
+class TivoliUserManager implements WebDav.ITestableUserManager {
+	constructor(
+		private readonly userService: UserService,
+		private readonly jwtService: JwtService,
+	) {}
+
+	getDefaultUser(callback: (user: WebDav.IUser) => void) {
+		console.log("getDefaultUser");
+		callback({
+			uid: "DefaultUser",
+			isAdministrator: false,
+			isDefaultUser: true,
+			username: "DefaultUser",
+			password: null,
+		});
+	}
+
+	private async tokenToUser(token: string): Promise<User> {
+		// jwt.strategy.ts
+		const payload = this.jwtService.decode(token) as JwtPayload;
+
+		if (
+			payload.t != JwtPayloadType.USER ||
+			payload.id == null ||
+			payload.exp == null
+		)
+			throw new Error("Invalid token");
+
+		const now = +new Date() / 1000;
+		if (now > payload.exp) throw new Error("Expired token");
+
+		const user = await this.userService.findById(payload.id);
+		if (user == null) throw new Error("Invalid token");
+
+		// auth.guard.ts
+		if (user.emailVerified == false) throw new Error("Unverified email");
+
+		return user;
+	}
+
+	getUserByNamePassword(
+		username: string,
+		password: string,
+		callback: (error: Error, user?: WebDav.IUser) => void,
+	) {
+		console.log("getUserByNamePassword");
+
+		this.tokenToUser(password)
+			.then(user => {
+				callback(null, {
+					uid: user.id,
+					isAdministrator: false,
+					isDefaultUser: false,
+					username: user.username,
+					password: null,
+					user,
+				} as WebDav.IUser);
+			})
+			.catch(error => {
+				callback(error);
+			});
+	}
+}
+
+class TivoliPrivilegeManager extends WebDav.PrivilegeManager {
+	constructor() {
+		super();
+	}
+
+	_can(
+		fullPath: WebDav.Path,
+		user: WebDav.IUser,
+		resource: WebDav.Resource,
+		privilege: string,
+		callback: WebDav.PrivilegeManagerCallback,
+	) {
+		callback(
+			null,
+			user.isDefaultUser == false && (user as any).user != null,
+		);
+	}
+}
+
+@Injectable()
+export class WebDavService implements OnModuleInit {
+	private readonly port = 4000;
+	private server: WebDav.WebDAVServer;
+
+	constructor(
+		//private readonly authService: AuthService,
+		private readonly filesService: FilesService,
+		private readonly userService: UserService,
+		private readonly jwtService: JwtService,
+	) {}
+
+	onModuleInit() {
+		//const userManager = new WebDav.SimpleUserManager();
+		//const user = userManager.addUser("username", "password", false);
+
+		// Privilege manager (tells which users can access which files/folders)
+		//const privilegeManager = new WebDav.SimplePathPrivilegeManager();
+		//privilegeManager.setRights(user, "/", ["all"]);
+
+		this.server = new WebDav.WebDAVServer({
+			port: this.port,
+
+			httpAuthentication: new WebDav.HTTPBasicAuthentication(
+				new TivoliUserManager(this.userService, this.jwtService),
+				"Tivoli Cloud VR",
+			),
+			privilegeManager: new TivoliPrivilegeManager(),
+			requireAuthentification: true,
+
+			//rootFileSystem: new FileSystem(this.filesService),
+			rootFileSystem: new WebDav.PhysicalFileSystem("/root"),
+		});
+
+		this.server.start(() =>
+			console.log("WebDav server running on *:" + this.port),
+		);
+	}
+}
