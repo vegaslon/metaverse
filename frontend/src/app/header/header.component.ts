c485857071ec1b9528d0b434846ede466bdf5a4d
Maki <mxmcube@gmail.com>
2020-03-24 18:56:31 +0000

    Added better mobile support

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,9 +1,16 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
+import {
+	Component,
+	OnDestroy,
+	OnInit,
+	ViewChild,
+	ElementRef,
+} from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Subscription } from "rxjs";
 import { AuthService, User } from "../auth/auth.service";
 import { SignInComponent } from "./sign-in/sign-in.component";
 import { DownloadComponent } from "./download/download.component";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
 	selector: "app-header",
@@ -11,19 +18,44 @@ import { DownloadComponent } from "./download/download.component";
 	styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent implements OnInit, OnDestroy {
-	private userSub: Subscription = null;
 	user: User = {} as User;
 
-	constructor(public dialog: MatDialog, private authService: AuthService) {}
+	private userSub: Subscription;
+	private routerEventsSub: Subscription;
+
+	@ViewChild("mobileMenu") mobileMenuRef: ElementRef<HTMLDivElement>;
+
+	constructor(
+		public readonly dialog: MatDialog,
+		private readonly authService: AuthService,
+		private readonly router: Router,
+	) {}
+
+	onToggleMobileMenu(forceClose = false) {
+		if (
+			this.mobileMenuRef.nativeElement.style.length > 0 &&
+			forceClose == false
+		) {
+			this.mobileMenuRef.nativeElement.removeAttribute("style");
+		} else {
+			this.mobileMenuRef.nativeElement.style.maxHeight = "0";
+			this.mobileMenuRef.nativeElement.style.padding = "0";
+		}
+	}
 
 	ngOnInit() {
 		this.userSub = this.authService.user$.subscribe(user => {
 			this.user = user;
 		});
+
+		this.routerEventsSub = this.router.events.subscribe(val => {
+			if (val instanceof NavigationEnd) this.onToggleMobileMenu(true);
+		});
 	}
 
 	ngOnDestroy() {
 		this.userSub.unsubscribe();
+		this.routerEventsSub.unsubscribe();
 	}
 
 	openDownload() {
