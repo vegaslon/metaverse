e2b7edfa9f55d2c3e5c5a36968da3d9911915975
Maki <mxmcube@gmail.com>
2020-04-08 19:21:13 +0100

    Fixed CurrentUser decorator to play nicely with domain tokens

--- server/src/auth/user.decorator.ts
+++ server/src/auth/user.decorator.ts
@@ -2,5 +2,7 @@ import { createParamDecorator } from "@nestjs/common";
 import { Request } from "express";
 
 export const CurrentUser = createParamDecorator((data, req: Request) => {
+	if (req.user == null) return null;
+	if ((req.user as any).username == null) return null; // req.user can be a domain
 	return req.user;
 });
