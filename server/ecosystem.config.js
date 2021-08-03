557baf05591833425b0a9d0b9d88eabde456ac4f
Maki <mxmcube@gmail.com>
2021-08-03 17:33:43 -0500

    Fixed undefined INSTANCES launch bug

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,8 +1,15 @@
+let instances = "max";
+
+if (process.env.INSTANCES != null) {
+	const n = parseInt(process.env.INSTANCES);
+	if (!Number.isNaN(n)) instances = n;
+}
+
 module.exports = {
 	apps: [
 		{
 			script: "dist/main.js",
-			instances: parseInt(process.env.INSTANCES) ?? "max",
+			instances,
 		},
 	],
 };
